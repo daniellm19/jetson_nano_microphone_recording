62278699d8b3339a456a1aa3fe4e4c24ec5235ef
Daniel <daniellm19@ru.is>
2022-03-25 17:06:21 +0000

    ok

--- record_microphone.py
+++ record_microphone.py
@@ -1,9 +1,15 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
+import os
+
+i = 0
+while os.path.exists("output%s.wav" % i):
+    i+=1
+
 
 fs = 44100  # Sample rate
 seconds = 3  # Duration of recording
 
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
 sd.wait()  # Wait until recording is finished
-write('output.wav', fs, myrecording)  # Save as WAV file 
+write('output%s.wav' % i, fs, myrecording)  # Save as WAV file 
